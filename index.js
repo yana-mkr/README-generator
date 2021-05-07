7561c87ec4d59d4619005fee55121b47403ba17f
yana-mkr <yana.mkrtchyan@gmail.com>
2021-05-06 22:24:22 -0500

    questions and first run function

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter directions to install your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Add additional usage information.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribuation guidelines for your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions for your project?'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(data => {
+            writeToFile("READMETEST.MD", generateMarkdown({ ...data }))
+        })
+}
 
 // Function call to initialize app
 init();
+
